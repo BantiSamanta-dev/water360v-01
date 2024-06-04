f8c5479c8cdcb6183b192a9a44cea918c43b07e1
Banti samanta <123482198+fathi0x@users.noreply.github.com>
2024-06-04 22:19:44 +0530

    card change

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ function Card() {
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 m-4">
       <button className="bg-green-500 p-4 rounded-md h-28 w-full hover:bg-indigo-700 shadow-xl flex flex-col justify-between">
         <p className="text-white font-semibold">Active Tender</p>
-        <p className="text-white font-bold text-2xl">52</p>
+        <p className="text-white font-bold text-2xl">10</p>
       </button>
       <button className="bg-gray-600 p-4 rounded-md h-28 w-full hover:bg-indigo-700 shadow-xl flex flex-col justify-between">
         <p className="text-white font-semibold">Past Tender</p>
@@ -13,11 +13,11 @@ function Card() {
       </button>
       <button className="bg-red-600 p-4 rounded-md h-28 w-full hover:bg-indigo-700 shadow-xl flex flex-col justify-between">
         <p className="text-white font-semibold">Live Tender</p>
-        <p className="text-white font-bold text-2xl">52</p>
+        <p className="text-white font-bold text-2xl">5</p>
       </button>
       <button className="bg-cyan-600 p-4 rounded-md h-28 w-full hover:bg-indigo-700 shadow-xl flex flex-col justify-between">
-        <p className="text-white font-semibold">Active Tender</p>
-        <p className="text-white font-bold text-2xl">52</p>
+        <p className="text-white font-semibold">Rejected Tender</p>
+        <p className="text-white font-bold text-2xl">2</p>
       </button>
     </div>
   );
